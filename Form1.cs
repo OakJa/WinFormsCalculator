564027fa5d0621e8289975964b8b6b1ecb0aedf3
OakJa <phutthiphong.n@kkumail.com>
2024-12-17 10:59:49 +0700

    lode.update

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿namespace WinFormsCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+            double iNum1 = Double.Parse(inputNum1);
+            double iNum2 = Double.Parse(inputNum2);
+            double iresult = iNum1 + iNum2;
+            result.Text = iresult.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+            double iNum1 = Double.Parse(inputNum1);
+            double iNum2 = Double.Parse(inputNum2);
+            double iresult = iNum1 * iNum2;
+            result.Text = iresult.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+            double iNum1 = Double.Parse(inputNum1);
+            double iNum2 = Double.Parse(inputNum2);
+            double iresult = iNum1 - iNum2;
+            result.Text = iresult.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string inputNum1 = num1.Text;
+            string inputNum2 = num2.Text;
+            double iNum1 = Double.Parse(inputNum1);
+            double iNum2 = Double.Parse(inputNum2);
+            double iresult = iNum1 / iNum2;
+            result.Text = iresult.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            num1.Clear();
+            num2.Clear();
+            result.Clear();
+        }
+
+        
+            
+        }
+    }
+
